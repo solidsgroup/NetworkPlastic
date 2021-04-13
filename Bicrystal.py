f131f3a0e766947b96cc66a7a78453a9a2bd0f56
dbugas <51766290+dbugas@users.noreply.github.com>
2021-04-13 13:42:49 -0700

    Added rotated interfaces and some changes to dadh function

--- Bicrystal.py
+++ Bicrystal.py
@@ -38,7 +38,6 @@ v1dot= 0
 v2dot = 0
 dh12 = 0
 dh21 = 0
-sgn = 0
 
 def CalF(sig):
     Ff = np.zeros((3,3))
@@ -67,10 +66,15 @@ def CalGibbs(grain):
                 G = G + 1/2*F[i,j]*P0[i,j] + P0[i,j]*Fgb2[i,j]             
     return G
 
+# set up rotation matrix
+theta = np.radians(0)
+c, s = np.cos(theta), np.sin(theta)
+rot2 = np.array(((c,0, s),(0,1,0), (-s,0, c)))
+Fgb12 = np.matmul(np.matmul(rot2,Fgb12),np.transpose(rot2))
+
 for t in range(tt):
     
     P0[0,1] = -.17*np.sin(8*m.pi/(tt)*t)
-    P0[1,0] = -.17*np.sin(8*m.pi/(tt)*t)
     
     G1 = CalGibbs(1)
     G2 = CalGibbs(2)
@@ -100,7 +104,6 @@ for t in range(tt):
         if(Fgb1[0,1] != 0):
             Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
     
-    sgn = np.sign(dh12)
     h12[t+1] = h12[t] + dt*dh12 
     v1dot = a12*dh12
     v2dot = -a12*dh12
@@ -155,7 +158,7 @@ import matplotlib.pyplot as plt
 import math as m
 # initualize values
 phi0 = 0.3
-phi1 = .5
+phi1 = 0.5
 C11,C12,C44 = 169.3097, 87.2201, 41.0448 # moduli for copper
 Cinv11,Cinv12,Cinv44 = 1/110, -0.34/110, 2*(1+0.34)/110 
 P0 = np.zeros((3,3)) # applied load
@@ -170,7 +173,7 @@ a12 = 1
 # yield at 1/8*phi0*fgb12^[0^(-1)]Cinv 
 
 tfin = .5
-dt = 0.00001
+dt = 0.000001
 tt = int(tfin/dt)
 V1 = np.zeros(tt+1)
 V2 = np.zeros(tt+1)
@@ -182,6 +185,8 @@ gib1 = np.zeros(tt+1)
 gib2 = np.zeros(tt+1)
 gib1[0] = 0
 gib2[0] = 0
+gib3 = np.zeros(tt+1)
+gib4 = np.zeros(tt+1)
 V1[0] = 0.5
 V2[0] = 0.5
 v1dot= 0
@@ -191,6 +196,7 @@ dh21 = 0
 sgn = 0
 
 def CalF(sig):
+    # calculates strain from a given sigma
     Ff = np.zeros((3,3))
     Ff[0,0] = Cinv11*sig[0,0] + Cinv12*sig[1,1]  + Cinv12*sig[2,2]
     Ff[1,1] = Cinv12*sig[0,0] + Cinv11*sig[1,1]  + Cinv12*sig[2,2]
@@ -203,8 +209,8 @@ def CalF(sig):
     Ff[0,2] = Ff[2,0]
     return Ff
 def CalSig(F):
+    # calculates sigma from a given strain
     sig = np.zeros((3,3))
-    
     sig[0,0] = C11*F[0,0] + C12*F[1,1]  + C12*F[2,2]
     sig[1,1] = C12*F[0,0] + C11*F[1,1]  + C12*F[2,2]
     sig[2,2] = C12*F[0,0] + C12*F[1,1]  + C11*F[2,2]
@@ -217,7 +223,7 @@ def CalSig(F):
     return sig
 
 def CalA(grain, V):
-    
+    # calculates A* for a grain
     A = 0
     if(grain == 1):
         temp = F0/V - 2.0*Fgb1
@@ -233,40 +239,41 @@ def CalA(grain, V):
                 A = A +  0.5*temp[i,j]*P[i,j]
     return A
     
-def CaldAdh (fgbpq, grain,v1,v2, up):
+def CaldAdh (fgbpq, grain,vp, up):
+    # calculates dA*/dh for a griven grain and sign of hpq
     dadh = 0
     if(grain == 1 and up == True):
-        temp = np.add(a12*Fgb1/v1**2, -a12*Fgb12/v1**2)
+        temp = np.add(a12*Fgb1/vp**2, -a12*Fgb12/vp**2)
         P1 = CalSig(F0)
         P2 = CalSig(temp)
         P3 = CalSig(Fgb1)
         for i in range(0,2):
             for j in range(0,2):
-                dadh = dadh + 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] - 4*a12/v1*fgbpq[i,j]*P3[i,j]
-    if(grain == 2and up == True):
-        temp = np.add(a12*Fgb2/v1**2, -a12*Fgb12/v1**2)
+                dadh = dadh + 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] - 4*a12/vp*fgbpq[i,j]*P3[i,j]
+    if(grain == 2 and up == True):
+        temp = np.add(a12*Fgb2/vp**2, -a12*Fgb12/vp**2)
         P1 = CalSig(F0)
         P2 = CalSig(temp)
         P3 = CalSig(Fgb2)
         for i in range(0,2):
             for j in range(0,2):
-                dadh = dadh + 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] - 4*a12/v2*fgbpq[i,j]*P3[i,j]
+                dadh = dadh + 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] - 4*a12/vp*fgbpq[i,j]*P3[i,j]
     if(grain == 1 and up == False):
-        temp = np.add(a12*Fgb12/v1**2, -a12*Fgb1/v1**2)
+        temp = np.add(a12*Fgb12/vp**2, -a12*Fgb1/vp**2)
         P1 = CalSig(F0)
         P2 = CalSig(temp)
         P3 = CalSig(Fgb1)
         for i in range(0,2):
             for j in range(0,2):
-                dadh = dadh - 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] + 4*a12/v1*fgbpq[i,j]*P3[i,j]
-    if(grain == 2and up == False):
-        temp = np.add(a12*Fgb12/v1**2, -a12*Fgb2/v1**2)
+                dadh = dadh - 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] + 4*a12/vp*fgbpq[i,j]*P3[i,j]
+    if(grain == 2 and up == False):
+        temp = np.add(a12*Fgb12/vp**2, -a12*Fgb2/vp**2)
         P1 = CalSig(F0)
         P2 = CalSig(temp)
         P3 = CalSig(Fgb2)
         for i in range(0,2):
             for j in range(0,2):
-                dadh = dadh - 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] + 4*a12/v2*fgbpq[i,j]*P3[i,j]
+                dadh = dadh - 1/(a12**3)*F0[i,j]*P1[i,j] - 2*F0[i,j]*P2[i,j] + 4*a12/vp*fgbpq[i,j]*P3[i,j]
     
     return dadh
     
@@ -278,28 +285,33 @@ Fgb12 = np.matmul(np.matmul(rot2,Fgb12),np.transpose(rot2))
 
 for t in range(tt):
     
-    F0[0,1] = .0008*np.sin(8*m.pi/(tt)*t)
-    F0[1,0] = .0008*np.sin(8*m.pi/(tt)*t)
+    F0[0,1] = .0008*np.sin(8*m.pi/(tt)*t) # applied strain
     
-    A1 = CalA(1,V1[t])
-    A2 = CalA(2,V2[t])
-    dadh1 = CaldAdh(Fgb12, 1,V1[t],V2[t],True)
-    dadh2 = CaldAdh(Fgb12, 2,V1[t],V2[t],True)
+    A1 = CalA(1,V1[t]) # A* for grain 1
+    A2 = CalA(2,V2[t]) # A* for grain 2
+    # dA/dhpq
+    dadh1 = CaldAdh(Fgb12, 1,V1[t],True) 
+    dadh2 = CaldAdh(Fgb12, 2,V2[t],True)
     dh12 = -(-A1*np.sign(v1dot) - A2*np.sign(v2dot) + V1[t]/a12*dadh1 + V2[t]/a12*dadh2 + phi0)/phi1
-    dadh1 = CaldAdh(Fgb12, 1,V1[t],V2[t],False)
-    dadh2 = CaldAdh(Fgb12, 2,V1[t],V2[t],False)
+    gib3[t+1] = -A1*np.sign(v1dot) - A2*np.sign(v2dot)
+    gib4[t+1] = V1[t]/a12*dadh1 + V2[t]/a12*dadh2
+    # dA/dhqp
+    dadh1 = CaldAdh(Fgb12, 1,V1[t],False)
+    dadh2 = CaldAdh(Fgb12, 2,V2[t],False)
     dh21 = -(A1*np.sign(v1dot) + A2*np.sign(v2dot) + (V1[t]/a12*dadh1 + V2[t]/a12*dadh2) + phi0)/phi1
     
-    gib1[t+1] = Fgb1[0,1]
-    gib2[t+1] = Fgb2[0,1]
+    gib1[t+1] = A1*np.sign(v1dot) + A2*np.sign(v2dot)
+    gib2[t+1] = V1[t]/a12*dadh1 + V2[t]/a12*dadh2
+    # stop if the gb interface moves more than 1/2 so that the volume does not become negative
     if (h12[t] < -1/2 or h12[t] > 1/2):
         dh12 = 0
         dh21 = 0
-
+    
     if(dh12 < 0):
         dh12 = 0
     if(dh21 < 0):
         dh21 = 0
+    # calculate eigenstrain 
     if(dh12 > 0):
         # V1 gets bigger
         Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
@@ -308,28 +320,28 @@ for t in range(tt):
         
     if(dh21 > 0):
         # V1 gets smaller 
-        dh12 = -dh21
+        dh12 = -dh21 # set hpq = hqp via antisymmetry 
         Fgb2 = np.add(Fgb2, -a12*dh12/V2[t]*dt*Fgb12)
         if(Fgb1[0,1] != 0):
             Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
     
-    sgn = np.sign(dh12)
-    h12[t+1] = h12[t] + dt*dh12
-    v1dot = a12*dh12
+    h12[t+1] = h12[t] + dt*dh12 # euler integration of interface position and volume
+    v1dot = a12*dh12 
     v2dot = -a12*dh12
     
     V1[t+1] = V1[t] + v1dot*dt 
     V2[t+1] = V2[t] + v2dot*dt 
     
     time[t+1] = t*dt
-
+    
+    # stress strain curve
     temp1 = np.add(1/V1[t]**2*F0, - 2*Fgb1) 
     temp2 = np.add(1/V2[t]**2*F0, - 2*Fgb2) 
     P1 = CalSig(temp1)
     P2 = CalSig(temp2)
     stress[t+1] = V1[t]*P1[0,1] + V2[t]*P2[0,1]
     strain[t+1] = F0[0,1] 
-    
+
 fig = plt.figure()
 ax = fig.add_subplot()
 plt.xlabel('Time')
@@ -360,8 +372,203 @@ plt.grid(alpha=.7,linestyle='-.')
 fig1, ax1 = plt.subplots()
 ax1.plot(time, gib1)
 ax1.plot(time, gib2)
+ax1.plot(time, gib3)
+ax1.plot(time, gib4)
+ax1.plot([0, tfin], [phi0, phi0])
+ax1.plot([0, tfin], [-phi0, -phi0])
 ax1.set_xlabel("Time")
-ax1.set_ylabel("Fgbp")
-ax1.set_title('Eigenstrain Change')
-ax1.legend(['Fgb1','Fgb2'])
+ax1.set_ylabel("Forces")
+ax1.set_title('Thermodynamic Forces')
+ax1.legend(['A*_qp','dA*/dhqp','A*_pq','dA*/dhpq'], bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.grid(alpha=.7,linestyle='-.')
+
+# %%  interface rotation - stress
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math as m
+
+th = [0, 30, 45, 60, 90]
+
+for i in range(5):
+    
+    phi0 = 0.1
+    phi1 = .02
+    Cinv11,Cinv12,Cinv44 = 1/110, -0.34/110, 2*(1+0.34)/110 # moduli for copper
+    P0 = np.zeros((3,3)) # applied load
+    Fgb12 = np.zeros((3,3)) # grain boundary shear
+    Fgb1 = np.zeros((3,3)) # eigenstrain for grain 1
+    Fgb2 = np.zeros((3,3)) # eigenstrain for grain 2
+    F0 = np.zeros((3,3)) # strain
+    F1 = np.zeros((3,3)) # strain for grain 1
+    F2 = np.zeros((3,3)) # strain for grain 2
+    Fgb12[0,1] = 0.6
+    a12 = 1
+    
+    tfin = 1.5
+    dt = 0.00001
+    tt = int(tfin/dt)
+    V1 = np.zeros(tt+1)
+    V2 = np.zeros(tt+1)
+    h12 = np.zeros(tt+1)
+    time = np.zeros(tt+1)
+    V1[0] = 0.5
+    V2[0] = 0.5
+    v1dot= 0
+    v2dot = 0
+    dh12 = 0
+    dh21 = 0
+    # set up rotation matrix
+    theta = np.radians(th[i])
+    c, s = np.cos(theta), np.sin(theta)
+    rot2 = np.array(((c,0, s),(0,1,0), (-s,0, c)))
+    Fgb12 = np.matmul(np.matmul(rot2,Fgb12),np.transpose(rot2))
+    
+    for t in range(tt):
+        
+        P0[0,1] = .25*np.sin(8*m.pi/(tt)*t)
+        
+        G1 = CalGibbs(1)
+        G2 = CalGibbs(2)
+        dh12 = -(-G1*np.sign(v1dot) - G2*np.sign(v2dot) - P0[0,1]*Fgb12[0,1] + phi0)/phi1
+        dh21 = -(G1*np.sign(v1dot) + G2*np.sign(v2dot) + P0[0,1]*Fgb12[0,1] + phi0)/phi1
+        
+        # stop if the gb interface moves more than 1/2 so that the volume does not become negative
+        if (h12[t] < -1/2 or h12[t] > 1/2):
+            dh12 = 0
+            dh21 = 0
+        
+        if(dh12 < 0):
+            dh12 = 0
+        if(dh21 < 0):
+            dh21 = 0
+        # calculate eigenstrain 
+        if(dh12 > 0):
+            # V1 gets bigger
+            Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
+            if(Fgb2[0,1] != 0):
+                Fgb2 = np.add(Fgb2, -a12*dh12/V2[t]*dt*Fgb12)
+            
+        if(dh21 > 0):
+            # V1 gets smaller 
+            dh12 = -dh21 # set hpq = hqp via antisymmetry 
+            Fgb2 = np.add(Fgb2, -a12*dh12/V2[t]*dt*Fgb12)
+            if(Fgb1[0,1] != 0):
+                Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
+        
+        h12[t+1] = h12[t] + dt*dh12 # euler integration of interface position and volume
+        v1dot = a12*dh12 
+        v2dot = -a12*dh12
+        
+        V1[t+1] = V1[t] + v1dot*dt 
+        V2[t+1] = V2[t] + v2dot*dt 
+        
+        time[t+1] = t*dt
+        
+    plt.plot(time, h12)
+plt.legend([r"$\theta = 0$",r"$\theta = \frac{\pi}{6}$",r"$\theta = \frac{\pi}{4}$",r"$\theta = \frac{\pi}{3}$",r"$\theta = \frac{\pi}{2}$"])
+plt.title('Stress driven motion with differing angle')
+plt.xlabel('Time')
+plt.ylabel('Change in h')
+plt.grid(alpha=.7,linestyle='-.')        
+plt.show()
+
+# %%  interface rotation - strain
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math as m
+
+th = [0, 30, 45, 60, 90]
+
+for i in range(5):
+    
+    # initualize values
+    phi0 = 0.3
+    phi1 = 0.5
+    C11,C12,C44 = 169.3097, 87.2201, 41.0448 # moduli for copper
+    Cinv11,Cinv12,Cinv44 = 1/110, -0.34/110, 2*(1+0.34)/110 
+    P0 = np.zeros((3,3)) # applied load
+    Fgb12 = np.zeros((3,3)) # grain boundary shear
+    Fgb1 = np.zeros((3,3)) # eigenstrain for grain 1
+    Fgb2 = np.zeros((3,3)) # eigenstrain for grain 2
+    F0 = np.zeros((3,3)) # strain
+    Fgb12[0,1] = 1
+    Fgb12[1,0] = 1
+    a12 = 1
+    
+    # yield at 1/8*phi0*fgb12^[0^(-1)]Cinv 
+    
+    tfin = .5
+    dt = 0.0001
+    tt = int(tfin/dt)
+    V1 = np.zeros(tt+1)
+    V2 = np.zeros(tt+1)
+    h12 = np.zeros(tt+1)
+    time = np.zeros(tt+1)
+    V1[0] = 0.5
+    V2[0] = 0.5
+    v1dot= 0
+    v2dot = 0
+    dh12 = 0
+    dh21 = 0
+    # set up rotation matrix
+    theta = np.radians(th[i])
+    c, s = np.cos(theta), np.sin(theta)
+    rot2 = np.array(((c,0, s),(0,1,0), (-s,0, c)))
+    Fgb12 = np.matmul(np.matmul(rot2,Fgb12),np.transpose(rot2))
+    
+    for t in range(tt):
+        
+        F0[0,1] = .0008*np.sin(8*m.pi/(tt)*t) # applied strain
+        
+        A1 = CalA(1,V1[t]) # A* for grain 1
+        A2 = CalA(2,V2[t]) # A* for grain 2
+        # dA/dhpq
+        dadh1 = CaldAdh(Fgb12, 1,V1[t],True) 
+        dadh2 = CaldAdh(Fgb12, 2,V2[t],True)
+        dh12 = -(-A1*np.sign(v1dot) - A2*np.sign(v2dot) + V1[t]/a12*dadh1 + V2[t]/a12*dadh2 + phi0)/phi1
+        # dA/dhqp
+        dadh1 = CaldAdh(Fgb12, 1,V1[t],False)
+        dadh2 = CaldAdh(Fgb12, 2,V2[t],False)
+        dh21 = -(A1*np.sign(v1dot) + A2*np.sign(v2dot) + (V1[t]/a12*dadh1 + V2[t]/a12*dadh2) + phi0)/phi1
+        
+        # stop if the gb interface moves more than 1/2 so that the volume does not become negative
+        if (h12[t] < -1/2 or h12[t] > 1/2):
+            dh12 = 0
+            dh21 = 0
+        
+        if(dh12 < 0):
+            dh12 = 0
+        if(dh21 < 0):
+            dh21 = 0
+        # calculate eigenstrain 
+        if(dh12 > 0):
+            # V1 gets bigger
+            Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
+            if(Fgb2[0,1] != 0):
+                Fgb2 = np.add(Fgb2, -a12*dh12/V2[t]*dt*Fgb12)
+            
+        if(dh21 > 0):
+            # V1 gets smaller 
+            dh12 = -dh21 # set hpq = hqp via antisymmetry 
+            Fgb2 = np.add(Fgb2, -a12*dh12/V2[t]*dt*Fgb12)
+            if(Fgb1[0,1] != 0):
+                Fgb1 = np.add(Fgb1, a12*dh12/V1[t]*dt*Fgb12)
+        
+        h12[t+1] = h12[t] + dt*dh12 # euler integration of interface position and volume
+        v1dot = a12*dh12 
+        v2dot = -a12*dh12
+        
+        V1[t+1] = V1[t] + v1dot*dt 
+        V2[t+1] = V2[t] + v2dot*dt 
+        
+        time[t+1] = t*dt
+        
+    plt.plot(time, h12)
+plt.legend([r"$\theta = 0$",r"$\theta = \frac{\pi}{6}$",r"$\theta = \frac{\pi}{4}$",r"$\theta = \frac{\pi}{3}$",r"$\theta = \frac{\pi}{2}$"])
+plt.title('Strain driven motion with differing angle')
+plt.xlabel('Time')
+plt.ylabel('Change in h')
+plt.grid(alpha=.7,linestyle='-.')        
+plt.show()
